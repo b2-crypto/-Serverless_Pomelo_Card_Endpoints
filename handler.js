1cc541202753443e1b283da5d471c5aad5dea079
Daniel <daoliverosd@gmail.com>
2024-03-08 16:16:43 -0500

    Add support for user createCard

--- handler.js
+++ handler.js
@@ -2,42 +2,63 @@ const pomelo = require("./code/pomelo_utils");
 const aws_dynamo = require("./code/aws_utils");
 const postgres = require("./code/postgres_utils");
 
+const MAX_CARD_NUMBER_PER_USER = 10;
+
 corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET,PUT,POST,PATCH,DELETE",
   "Access-Control-Allow-Headers":
     "Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token, X-Amz-User-Agent, X-Amzn-Trace-Id",
+};
+
+async function searchCards(event) {
+  username = event.requestContext.authorizer.jwt.claims.sub;
+  cards = await postgres.searchCards(username);
+
+  return {
+    statusCode: 200,
+    corsHeaders,
+    body: JSON.stringify(
+      {
+        cards: cards.rows,
+      },
+      null,
+      2
+    ),
+  };
 }
 
-  async function searchCards(event) {
-    username = event.requestContext.authorizer.jwt.claims.sub;
-    cards = await postgres.searchCards(username);
-
+async function createCard(event) {
+  username = event.requestContext.authorizer.jwt.claims.sub;
+  user = await aws_dynamo.getUser(username);
+  cards = await postgres.searchCards(username);
+  if (cards.rowCount > MAX_CARD_NUMBER_PER_USER) {
     return {
-      statusCode: 200,
+      statusCode: 500,
       corsHeaders,
       body: JSON.stringify(
         {
-          cards: cards.rows,
+          message: "Maximum number of card per user reached",
+          data: responseJson,
         },
         null,
         2
       ),
     };
-  };
+  }
 
-async function createCard(event) {
-  body = event.body;
-  username = await aws_dynamo.getUserUsingPomeloID(JSON.parse(body)['user_id'])
-  response = await pomelo.createCard(body);
+  pomelo_userId = user.PomeloUserID.S;
+
+  body = JSON.parse(event.body);
+  body.user_id = pomelo_userId;
+  response = await pomelo.createCard(JSON.stringify(body));
   responseJson = JSON.parse(response);
   postgresCard = await postgres.proccessCreateCard(
     responseJson.data.id,
     0,
     "Pomelo",
-    username["Items"][0]["id"]['S'],
+    username
   );
-
   return {
     statusCode: 200,
     corsHeaders,
